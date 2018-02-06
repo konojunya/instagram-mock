fdc89acfab8e993316b11689bfd2e7700d62bc67
konojunya <junya03dance@gmail.com>
2018-02-07 00:44:36 +0900

    めんどくせぇから明日する

---
+++ src/app.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import {
+    View,
+    Text
+} from 'react-native'
+
+export default class App extends React.Component<{}, {}> {
+
+    render(){
+        return(
+            <View>
+                <Text>Hello</Text>
+            </View>
+        )
+    }
+
+}
